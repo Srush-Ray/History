3e4c57808d00064c598ad383a3fc82017d9f10c6
Srushti <srushti.raybhoge1999@gmail.com>
2020-11-07 12:53:38 +0530

    changed

---
+++ build/precache-manifest.19991cf2a99346b1987c34afad97e9af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "492733fd1f1e2ce0cb4020e5620bdbb7",
+    "url": "/HistoryObjectives/index.html"
+  },
+  {
+    "revision": "66eaaf23eba6987e4abf",
+    "url": "/HistoryObjectives/static/css/main.ef8d6706.chunk.css"
+  },
+  {
+    "revision": "411128b3c08b34738086",
+    "url": "/HistoryObjectives/static/js/2.ade06c19.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/HistoryObjectives/static/js/2.ade06c19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66eaaf23eba6987e4abf",
+    "url": "/HistoryObjectives/static/js/main.714dc5fc.chunk.js"
+  },
+  {
+    "revision": "bd779fff5e372c6825c3",
+    "url": "/HistoryObjectives/static/js/runtime-main.8b0c53fc.js"
+  },
+  {
+    "revision": "76b4cafe11d2788afe5a344e647e5448",
+    "url": "/HistoryObjectives/static/media/Register.76b4cafe.svg"
+  }
+]);
