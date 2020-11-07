82835d293991725f41e236c11eea7f43042817b8
Srushti <srushti.raybhoge1999@gmail.com>
2020-11-07 12:24:08 +0530

    initial commit

---
+++ build/precache-manifest.13521fc3608a4f747f3f5755219d90ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bca49d94499f6e016912b7ad5bd12e75",
+    "url": "/History/index.html"
+  },
+  {
+    "revision": "b99a8b49014a4635ed92",
+    "url": "/History/static/css/main.ef8d6706.chunk.css"
+  },
+  {
+    "revision": "411128b3c08b34738086",
+    "url": "/History/static/js/2.ade06c19.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/History/static/js/2.ade06c19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b99a8b49014a4635ed92",
+    "url": "/History/static/js/main.9049bc23.chunk.js"
+  },
+  {
+    "revision": "cbbb343fc8e6589838a9",
+    "url": "/History/static/js/runtime-main.56e00664.js"
+  },
+  {
+    "revision": "76b4cafe11d2788afe5a344e647e5448",
+    "url": "/History/static/media/Register.76b4cafe.svg"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+// import logo from './logo.svg';
+// import './App.css';
+import Objective from "./Objectives/Objective";
+import History from "./History/History.js";
+
+function App() {
+  return (
+    <div className="App">
+    {/*
+      <History />
+    */}
+    <Objective />
+    </div>
+  );
+}
+
+export default App;
