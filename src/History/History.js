0e8855047d545117ff42b9b3de75cf608cdc415c
Srushti <srushti.raybhoge1999@gmail.com>
2020-12-21 22:58:05 +0530

    new changes

--- src/History/History.js
+++ src/History/History.js
@@ -24,47 +24,116 @@ class History extends React.Component {
     super();
     this.state = {
       text: [
-        <p style={{ fontSize: "42px" }}>
+        <p className="History__lightColor History__text1">
           National <br /> Integrated Medical
           <p style={{ color: "#e0a984" }}>Association</p>
         </p>,
-        <p className="textFont" style={{ color: "#ffdfc9" }}>
+        <p className="History__textFont" style={{ color: "#FFEADC" }}>
           The National Integrated Medical Association (NIMA) is an{" "}
           <span style={{ color: "#FF812D" }}>
-            Indian Non-governmental organization established in 1948.{" "}
+            {" "}
+            Indian Non-governmental organization established in 1948.
           </span>{" "}
-          Integrated medical education was started nearly 75 years ago, and
-          though the graduates coming out of these courses proved very
-          successful practitioners ideal for the conditions of this country, the
-          vested interests from both modern medical Ayurved. Ayurvedcamps,
-          misguided the authorities and created various problems for integrated
-          practitioners from the very beginning. Gradually the integrated
-          medical community started organising itself to face the problems in an
-          organised manner
+          Integrated medical education was started nearly
+          <br />
+          <span className="History__75" style={{ color: "#FF812D" }}>
+            {" "}
+            75 <span style={{ fontSize: "16px" }}> years ago </span>{" "}
+          </span>
+          , and though the graduates coming out of these courses proved very
+          successful practitioners ideal for the conditions of this country.
+        </p>,
+        <p className="History__textFont" style={{ color: "#FFEADC" }}>
+          The vested interests from both
+          <span style={{ color: "#FF812D" }}>
+            {" "}
+            modern medical & pure Ayurved camps,
+          </span>{" "}
+          <span
+            className="History__text2"
+            style={{ color: "#F9A162", fontWeight: "500" }}
+          >
+            misguided the authorities{" "}
+            <span
+              className=""
+              style={{ color: "#F9A162", fontWeight: "normal" }}
+            >
+              and
+            </span>{" "}
+            created various problems
+          </span>{" "}
+          <span className="History__textFont" style={{ color: "#FFEADC" }}>
+            for integrated practitioners from the very beginning. Gradually the
+            integrated medical community started organising itself to face the
+            problems in an organised manner
+          </span>
         </p>,
         <p style={{ color: "#ffdfc9", fontSize: "42px" }}>
           All <br /> India <p style={{ color: "#f9b384" }}>Conference</p>{" "}
         </p>,
-        <p style={{ fontSize: "13px" }}>
+        <p className="History__text3">
           The first major attempt was of the establishment of National Medical
-          Association of India in 1947, which soon spread its activities in
-          northern states of the country. From 1953 onwards other Associations
-          of Integrated Practitioners were established in the states of Bombay,
-          Mysore with the names as{" "}
-          <span style={{ color: "orange" }}>
+          Association of India in{" "}
+          <span
+            className="History__75"
+            style={{ color: "#FF812D", lineHeight: "1.5" }}
+          >
+            {" "}
+            1947
+          </span>{" "}
+          , which soon spread its activities in northern states of the country.
+          From
+          <span
+            className="History__75"
+            style={{ color: "#FF812D", lineHeight: "1.5" }}
+          >
+            {" "}
+            1953
+          </span>{" "}
+          onwards other Associations of Integrated Practitioners were
+          established in the states of Bombay, Mysore and madras,etc. with the
+          names as{" "}
+          <span style={{ color: "#F9A162" }}>
             Bombay State Integrated Medical Association
           </span>{" "}
           as{" "}
           <span style={{ color: "#FF812D" }}>
-            Maharashtra State Integrated Medical Association &amp; Gujrat State
+            Maharashtra State Integrated Medical Association & Gujrat State
             Integrated Medical Association, Integrated Associations of Mysore,
             etc.
           </span>
+        </p>,
+        <p style={{ fontSize: "13px" }}>
           The attempt to bring all these associations together, materialised in
-          1966 at 5th AIC of NIMA Conference under the Presidentship of Dr Panna
-          Lal Varshney and at the 7th All India Conference of NIMA held at
-          Bhopal on 1.2.1969 the Constitution of the National Integrated Medical
-          Association was adopted and thus NIMA was born.
+          <span
+            className="History__75"
+            style={{ color: "#FF812D", lineHeight: "1.5" }}
+          >
+            {" "}
+            1966{" "}
+          </span>
+          at
+          <span style={{ color: "#F9A162", fontSize: "17px" }}>
+            {" "}
+            5th AIC{" "}
+          </span>{" "}
+          of NIMA Conference under the Presidentship of
+          <span style={{ color: "#FF812D", fontSize: "17px" }}>
+            {" "}
+            Dr Panna Lal Varshney
+          </span>{" "}
+          and at the{" "}
+          <span style={{ color: "#F9A162", fontSize: "17px" }}>7th</span> All
+          India Conference of NIMA held at Bhopal on
+          <span style={{ color: "#FF812D", fontSize: "17px" }}> 1.2.1969 </span>
+          the Constitution of the National Integrated Medical Association was
+          adopted and <br />
+          thus{" "}
+          <span style={{ color: "#FF812D", fontSize: "26px", lineHeight: "2" }}>
+            {" "}
+            NIMA
+          </span>{" "}
+          <span style={{ color: "#F9A162", fontSize: "17px" }}>was born.</span>
         </p>,
         <p>
           <p style={{ color: "#ffdfc9", fontSize: "70px" }}> 1st</p>
@@ -91,15 +160,27 @@ class History extends React.Component {
         <p
           style={{
             color: "#ffdfc9",
+            fontSize: "13px",
           }}
-          className="textFont"
+          // className="textFont"
         >
-          Today NIMA is the only all India organisation of the institutionally
-          trained and qualified practitioners of Indian Systems of Medicine, who
-          have undergone the conjoint study of Ayurveda, Unani, Siddha & modern
-          medicine, for a period of at least four years after Inter Science or
-          XIIth std (10 + 2 years education). The Association has well organised
-          branches in 17 states and in union territories of Delhi & Chandigarh.
+          Today NIMA is the only all India organisation of the
+          <span style={{ color: "#F9A162" }} className="History__text4">
+            {" "}
+            institutionally trained and qualified practitioners{" "}
+          </span>
+          of Indian Systems of Medicine, who have undergone the conjoint study
+          of Ayurveda, Unani, Siddha & modern medicine, for a period of at least
+          four years after Inter Science or XIIth std (10 + 2 years education).
+          The Association has well organised branches in{" "}
+          <span style={{ color: "#F9A162", fontSize: "18px" }}>
+            {" "}
+            <span style={{ color: "#E77428" }} className="History__17">
+              17
+            </span>{" "}
+            states and in union territories{" "}
+          </span>
+          of Delhi & Chandigarh.
         </p>,
         <p style={{ fontSize: "44px" }}>
           <span style={{ color: "orange" }}>Branch</span>es
@@ -108,7 +189,7 @@ class History extends React.Component {
           style={{
             color: "#ffdfc9",
           }}
-          className="textFont"
+          className="History__textFont"
         >
           Today NIMA is the only all India organisation of the institutionally
           trained and qualified practitioners of Indian Systems of Medicine, who
@@ -119,66 +200,155 @@ class History extends React.Component {
         </p>,
         <p>
           <span
-            style={{ color: "#ffdfc9", fontSize: "100px", lineHeight: "0.5" }}
+            style={{ color: "#ffdfc9", fontSize: "100px", lineHeight: "1.2" }}
           >
             lll
           </span>
           <br />
-          <span style={{ color: "#ffdfc9", fontSize: "30px" }}>Tier</span>{" "}
+          <span
+            style={{ color: "#ffdfc9", fontSize: "30px", lineHeight: "1.2" }}
+          >
+            Tier
+          </span>{" "}
           <br />
-          <span style={{ color: "#ffb482", fontSize: "30px" }}>
+          <span
+            style={{ color: "#ffb482", fontSize: "30px", lineHeight: "1.2" }}
+          >
             System of{" "}
           </span>{" "}
           <span style={{ color: "#ff812d", fontSize: "30px" }}>NIMA </span>
         </p>,
         <p style={{ color: "#ffdfc9", fontSize: "12px" }}>
-          <p
-            style={{ fontWeight: "bolder", color: "#ffdfc9", fontSize: "16px" }}
-          >
+          <p style={{ fontWeight: "bold", color: "#FF812D", fontSize: "17px" }}>
             The Association has a three tier system in its working
           </p>
           Local/District Branch.
           <br />
+          <br />
           State Branches/Territorial Branches.
+          <br />
           <br />
           Central Council.
         </p>,
-        <p style={{ color: "#ffdfc9", fontSize: "14px" }} className="textFont">
-          Minimum 11 eligible integrated practitioners from any town/city/area
-          can come together and function locally as per the Constitution of
-          NIMA.
-          <p>
-            All Local/District Branches in a state form a State Branch of NIMA,
-            which is governed by its state council. Local Branch Managing
-            Committees send their representatives in proportion to the number of
-            their members to the State Council. The State Council meets twice a
-            year, elect its Office Bearers and carries out its duties for the
-            entire State. The details regarding the working of the
-            Local/District Branch and State Branches can be seen in the booklet
-            of By-Laws of NIMA
-          </p>
+        <p className="History__textFont">
+          Minimum{" "}
+          <span
+            style={{ color: "#E77428", fontWeight: "bold" }}
+            className="History__17"
+          >
+            11
+          </span>{" "}
+          eligible integrated practitioners from{" "}
+          <span style={{ color: "#E77428" }} className="History__17">
+            any town/city/area
+          </span>{" "}
+          can come together and function locally as per the
+          <span style={{ color: "#E77428", fontWeight: "bold" }}>
+            {" "}
+            Constitution of NIMA.
+          </span>
+          <br />
+          <br />
+          All{" "}
+          <span style={{ color: "#E77428", fontSize: "19px" }}>
+            Local/District
+          </span>{" "}
+          Branches in a state form a State Branch of NIMA, which is governed by
+          its{" "}
+          <span style={{ color: "#E77428", fontSize: "19px" }}>
+            state council.
+          </span>
         </p>,
-        <p className="textFont" style={{ color: "#ffdfc9" }}>
-          The Central Council is formed by the representatives coming from all
-          the State Councils every year. In proportion to their membership
-          strength. The state Secretary is necessarily the first representative
-          of the State Branch on the Central Council. The Central Council is the
-          important body in which are vested the general control, management and
-          direction of policy of the Association. The central council in its
-          annual meeting elects the Central Office bearers who form the
-          Executive Committee of NIMA which executes the programme laid down by
-          the Central Council.
+        <p style={{ color: "#ffdfc9", fontSize: "14px" }} className="textFont">
+          Local Branch Managing Committees send their representatives in
+          proportion to the number of their members to the{" "}
+          <span style={{ color: "#FF812D", fontSize: "20px" }}>
+            State Council.
+          </span>{" "}
+          The State Council meets twice a year, elect its Office Bearers and
+          carries out its duties for the entire State. The details regarding the
+          working of the Local/District Branch and State Branches can be seen in
+          the booklet of{" "}
+          <span style={{ color: "#FF812D", fontSize: "20px" }}>
+            By-Laws of NIMA
+          </span>
+        </p>,
+        <p style={{ color: "#ffdfc9", fontSize: "14px" }}>
+          <span style={{ color: "#FF812D", fontSize: "18px" }}>
+            The Central Council
+          </span>{" "}
+          is formed by the representatives coming from all the State Councils
+          <span style={{ color: "#FF812D", fontSize: "18px" }}>
+            {" "}
+            every year.
+          </span>{" "}
+          In proportion to their membership strength. The
+          <span style={{ color: "#FF812D", fontSize: "18px" }}>
+            {" "}
+            STATE SECRETARY
+          </span>{" "}
+          is necessarily the first representative of the
+          <span style={{ color: "#FF812D", fontSize: "18px" }}>
+            {" "}
+            State Branch
+          </span>{" "}
+          on the
+          <span style={{ color: "#FF812D", fontSize: "18px" }}>
+            {" "}
+            Central Council.
+          </span>{" "}
+        </p>,
+        <p style={{ color: "#FFEADC", fontSize: "14px" }}>
+          The Central Council is the important body in which are vested the
+          general control, management and direction of policy of the
+          Association. The central council in its annual meeting elects the
+          <span style={{ color: "#F79D60", fontSize: "19px" }}>
+            {" "}
+            Central Office bearers
+          </span>{" "}
+          who form the
+          <span style={{ color: "#F79D60", fontSize: "18px" }}>
+            {" "}
+            Executive Committee
+          </span>{" "}
+          of NIMA which executes the programme laid down by the Central Council.
         </p>,
         <p style={{ color: "#ffdfc9", fontSize: "40px" }}>Membership</p>,
         <p style={{ fontSize: "13px", color: "#ffdfc9" }}>
           To bring financial stability and to have labour of workers on
           enrolments of annual members, the Association has laid stress on
-          enrolling maximim life members. The life membership subscription is
-          kept in long term fixed deposit in a Nationalised Bank and the annual
-          interest accrued on it is distributed. The Association has opened
-          Associate Membership for non integrated qualified practitioners who
-          believe in integration. The Association year for Branches is from 1
-          October to 30 September
+          enrolling maximim
+          <span style={{ color: "#FF812D", fontSize: "20px" }}>
+            {" "}
+            life members.
+          </span>{" "}
+          The life membership subscription is kept in
+          <span style={{ color: "#F9A162", fontSize: "20px" }}>
+            {" "}
+            long term fixed deposit in a Nationalised Bank
+          </span>{" "}
+          and
+          <span style={{ color: "#F9A162", fontSize: "20px" }}>
+            {" "}
+            the annual interest{" "}
+          </span>
+          accrued on it is distributed.
+        </p>,
+        <p style={{ fontSize: "14px" }}>
+          The
+          <span style={{ color: "#F9A162", fontSize: "22px" }}>
+            {" "}
+            Association has opened Associate Membership for non integrated{" "}
+          </span>
+          qualified practitioners who believe in integration. The Association
+          year for Branches is from
+          <br />
+          <span style={{ color: "#FF812D", fontSize: "22px" }}>
+            {" "}
+            <span style={{ fontSize: "37px" }}>1</span> October{" "}
+            <span style={{ fontSize: "14px" }}>to</span>{" "}
+            <span style={{ fontSize: "37px" }}>30</span> September
+          </span>
         </p>,
       ],
       currentText: 0,
@@ -273,29 +443,22 @@ class History extends React.Component {
   render() {
     return (
       <div className="container">
-        <div className="row">
+        <div className="row History__row">
           <div className="text">
             <p id="paragraphContent">
-              {this.state.text[this.state.currentText]}
+              {
+                this.state.text[this.state.currentText]
+                // this.state.text[20]
+              }
             </p>
           </div>
 
           <hr id="lineDivider" />
         </div>
-        <div className="row registerLink">
-          <a
-            className="link"
-            href="#"
-            style={{
-              color: "white",
-              // textAlign: "center!",
-              textDecoration: "none",
-              // marginRight: "0px",
-              fontSize: "15px",
-              // float: "right",
-            }}
-          >
-            Register Yourself <img height="18px" src={Register} alt="" />
+        <div className="row History__registerLink">
+          <a className="History__link" href="#">
+            Register Yourself{" "}
+            <img src={Register} alt="" className="History__linkImg img-fluid" />
           </a>
         </div>
         <div className="MobileView">
